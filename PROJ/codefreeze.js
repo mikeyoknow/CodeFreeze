b6abe19a29a9d5399df52451a0e4c6c23056635f
mikeyoknow <114947956+mikeyoknow@users.noreply.github.com>
2022-12-07 08:20:48 -0500

    final changes

--- PROJ/codefreeze.js
+++ PROJ/codefreeze.js
@@ -1,182 +1,113 @@
-/*
-Web-based diary - js
-EECS1012 - PROJECT
-Authors: CodeFreeze(Erfan, Idil, William and Marwa)
-*/
+var i = 1; //default number of passages
 
-var clicked=1;
-var password;
-var pass;
-let prm = false;
-var i = 0;
-
-
-
-//onmouseover functions
-function on1(){
-    let button = document.getElementById("save");
-    button.style.backgroundColor = "lightgreen";
-    button.style.color = "white";
-    
-}
-function on2(){
-    let button = document.getElementById("lock");
-
-    if(clicked==0){
-        button.style.backgroundColor = "red";
-        button.style.color = "white";
-    }
-    if(clicked==1){
-        button.style.backgroundColor = "lightgreen";
-        button.style.color = "white";
-    }
- 
-}
-function on3(){
-    let button = document.getElementById("img");
-    button.style.backgroundColor = "lightgreen";
-    button.style.color = "white";
-}
-function on4(){
-    let button = document.getElementById("NightMode");
-    button.style.backgroundColor = "lightgreen";
-    button.style.color = "white";
-}
-function on5(){
-    let button = document.getElementById("add");
-    button.style.backgroundColor = "lightgreen";
-    button.style.color = "white";
-}
-function on6(){
-    let button = document.getElementById("LightMode");
-    button.style.backgroundColor = "lightgreen";
-    button.style.color = "white";
-}
-//onmouseout functions
-function out1(){
-    let button = document.getElementById("save");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function out2(){
-    let button = document.getElementById("lock");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function out3(){
-    let button = document.getElementById("img");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function out4(){
-    let button = document.getElementById("NightMode");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function out5(){
-    let button = document.getElementById("add");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function out6(){
-    let button = document.getElementById("LightMode");
-    button.style.backgroundColor = "white";
-    button.style.color = "black";
-}
-function starter() {
-    let username = sessionStorage.getItem('username');
-
-    if (username === null) {
-        username = prompt("To make this diary more personal to YOU, please enter your name");
-    }
-
-    if (username != null) {
-        document.getElementById("name").innerHTML = "Welcome " + username;
-        sessionStorage.setItem('username', username);
-    } else {
-        document.getElementById("noUser").innerHTML = "Welcome, Stranger!";
+function takeback(){ 
+    var x = document.getElementById("userPassword").value;
+    if(x==""){
+        alert("please fill out the field!")
+    }else{
+        sessionStorage.setItem('password', x)
+        location.href = "login.html";
     }
 }
-function lock(){
-    window.location="codefreeze.html";
+
+function transfer(){
+    var x = document.getElementById("userPassword").value;
+    let password = sessionStorage.getItem('password');
+    if(x==""){
+        alert("please fill out the field!")
+    }else{
+        if(password==x){
+            location.href = "codefreeze.html";
+        }else{
+            alert("Password is wrong");
+        }
+    }
 }
-function darkMode() {
-    var element = document.getElementById("main");
-    element.style.backgroundColor = "black";
 
-    var head = document.getElementById("header");
-    head.style.backgroundColor = "black"
-    head.style.borderColor = "white";
-
-    var name = document.getElementById("name")
-    name.style.color = "white"
-
-    var date = document.getElementById("date")
-    date.style.color = "white"
-
-    var butt = document.getElementById("buttons")
-    butt.style.backgroundColor = "black";
-    butt.style.borderColor = "white"; 
-}
-function lightMode() {
-    var element = document.getElementById("main");
-    element.style.backgroundColor = "#beefc4";
-
-    var head = document.getElementById("header");
-    head.style.backgroundColor = "lightblue"
-    head.style.borderColor = "black";
-
-    var name = document.getElementById("name")
-    name.style.color = "black"
-
-    var date = document.getElementById("date")
-    date.style.color = "black"
-
-    var butt = document.getElementById("buttons")
-    butt.style.backgroundColor = "rgb(240, 202, 209)";
-    butt.style.borderColor = "black";
-}
-function add(){
-    var newP = document.createElement("textarea")
-    i = i+1;
-    $(newP).attr("id","passage"+i);
-    $(newP).attr("name","form"+i);
-    $(newP).attr("rows","4");
-    $(newP).attr("cols","50");
-    $(newP).attr("innerHTML", "passage#"+i);
-
-    $(newP).css({'backgroundColor':'lightyellow'})
-    $(newP).css({'font-family':'Schoolbell','arial':'serif'});
-    $(newP).css({'font-size':'25px'});
-    $(newP).css({'padding':'10px'});
-    $(newP).css({'text-align':'left'});
-    $(newP).css({'border':'3px'});
-    $(newP).css({'borderColor':'black'});
-    $(newP).css({'border':'solid'});
-    $(newP).css({'width': '100%'});
-    $(newP).css({'box-sizing': 'border-box'});
-
+function DATE(){
     n =  new Date();
     y = n.getFullYear();
     m = n.getMonth() + 1;
     d = n.getDate();
-    newP.innerHTML = m + "/" + d + "/" + y
+    return m + "/" + d + "/" + y;
 
-    $(document.getElementsByClassName("passage")).append(newP);
+}
+
+function Name(){
+    let username = sessionStorage.getItem('username');
+
+if (username === null) {
+    username = prompt("To make this diary more personal to YOU, please enter your name");
+}
+
+if (username != null) {
+    document.getElementById("name").innerHTML = username;
+    sessionStorage.setItem('username', username);
+} else {
+    document.getElementById("name").innerHTML = "Welcome, Stranger!";
+}
+}
+
+function del(){
+    var res = prompt("Which pasage do you want to delete?")
+    if(res>i){
+        alert("Passage doesn't exist");
+    }else{
+        document.getElementById("psg"+res).style.display = "none";
+        document.getElementById("lbl"+res).style.display = "none";
+    }
+}
+
+function add(){
+    i=i+1;
+    var user = document.getElementById("user");
+
+    user.append(document.createElement('br'));
+
+    var newL = document.createElement('label');
+    newL.setAttribute('id','lbl'+i);
+    newL.innerHTML = "Passage #"+i;
+    user.append(newL);
+
+    user.append(document.createElement('br'));
+
+    var newT = document.createElement('textarea');
+    newT.setAttribute('id', 'psg'+i);
+    newT.setAttribute('rows', '10');
+    newT.setAttribute('cols', '30');
+    user.append(newT);
 }
 
 function save(){
+    var tempEntryList = [];
+    var tempID;
+    var tempPassage;
 
-}
-function del(){
-    alert("which passage do you want to delete? You have "+i+" passage so far")
-    var res = prompt("Write the passage number you'd like to delete:")
-    
-    if(res>i || res<1){
-        alert("This passage doesn't exist!")
-    }else{
-        document.getElementById("passage"+res).style.display = "none";
-        alert("passage"+res+" has been deleted!");
+    for(var j = 1; j< i; j++){
+        tempID =  document.getElementsByClassName("passage")[j].id;
+        tempPassage = document.getElementsByClassName("passage")[j].value;
+        tempEntryList.push({'id':tempID, 'content':tempPassage});
     }
-    
+    userDataSheet['diaryEntryList'] = tempEntryList;
+    console.log(tempEntryList);
+    userDataSheetUpdate();
+}
+function lock(){
+    location.href = "login.html";
+}
+
+function userDataSheetUpdate(){
+    //var username = document.getElementById("username").value;
+    //var password = document.getElementById("password").value;
+
+    console.log(userDataSheet);
+    var tempDataSheet = userDataSheet;
+    tempDataSheet['diaryEntryList'] = encryptEntries(tempDataSheet['diaryEntryList'], password);
+    $.post(url+'?data='+JSON.stringify({
+        'username':encrypt(username, password), 
+        'action':'userDataSheetUpdate',  
+        'userDataSheet': tempDataSheet,
+        'password':hash(password)}),
+        response
+    );
 }
---
+++ PROJ/codefreeze_server.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at port 3000!'));
+
+app.use(express.static('codefreeze.html'));
+
+/*
+
+const userDataSheet = {
+    username: "",
+    password: "Doe", //hashed
+    diaryEntryList: []
+};
+
+const diaryEntry = {
+    id: 0,
+    title: "",
+    content: ""
+};
+
+*/
+
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/login.html");
+    console.log("Get Page");
+});
+
+app.post('/post', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    var z = JSON.parse(req.query['data']);
+    console.log(z);
+    var username = z['username'];
+    var password = z['password'];
+    var action = z['action'];
+
+    if (action == 'login') {
+        if(!isUserValid(username)){
+            var jsontext = JSON.stringify({'action':'userNotFound'});
+            console.log("Usernotvalid");
+        }
+        else if(isPasswordValid(username, password)){
+            var jsontext = JSON.stringify({'action':'loginSuccess'});
+            console.log("passvalid");
+        }else{
+            var jsontext = JSON.stringify({'action':'loginFailed'});
+            console.log("Loginfail");
+        }
+        res.send(jsontext);
+
+    }else if (action == 'userDataSheetRequest') {
+
+        var jsontext = JSON.stringify({
+            'action':'userDataSheetReturn',
+            'userDataSheet': readUserDataSheet(username)
+        });
+        console.log("datasheetrequest");
+        res.send(jsontext);
+
+    }else if (action == 'userDataSheetUpdate') {
+
+        try{
+            updateUserDataSheet(username, z['userDataSheet']);
+        }catch(err){
+            console.log(err);
+            res.send(JSON.stringify({ 'msg': 'ERROR' }));
+        }
+        var jsontext = JSON.stringify({'action':'dataUpdateSuccess'});
+        console.log("datasheetupdate");
+        res.send(jsontext);
+
+    }else if (action == 'createNewAccount') {
+
+        if(isUserValid(username)){
+            console.log("userexists");
+            var jsontext = JSON.stringify({'action':'userExists'});
+        }else{
+            createNewUser(username, password);
+            console.log("accountcreated");
+            var jsontext = JSON.stringify({'action':'accountCreated'});
+        }
+        res.send(jsontext);
+
+    }else{
+        res.send(JSON.stringify({ 'msg': 'ERROR' }));
+    }
+
+
+}).listen(port);
+console.log("Server is running! (listening on port " + port + ")");
+
+function initialize(){
+
+}
+
+function createNameList(){
+
+}
+
+function isUserValid(username){
+    var fs = require('fs');
+    try {
+        fs.readFileSync('UserData/'+username+'.txt', 'utf8');
+    } catch (err) {
+        return false;
+    }
+    return true;
+}
+
+function isPasswordValid(username, password){
+    //console.log(readUserDataSheet(username)["password"]);
+    //console.log(password);
+    if(readUserDataSheet(username)["password"] === password){
+        return true;
+    }
+    return false;
+}
+
+function getUserList(){
+    var userList = [];
+    return userList;
+}
+
+function updateUserDataSheet(username, userDataSheet){
+    var fs = require('fs');
+    try {
+        fs.writeFile('UserData/'+username+'.txt', JSON.stringify(userDataSheet), function (err) {if (err) throw err;console.log('Updated!');});
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+function createNewUser(username, password){
+    var fs = require('fs');
+    try {
+        var newDataSheet = {'username':username, 'password': password, 'diaryEntryList': []};
+        fs.writeFile('UserData/'+username+'.txt', JSON.stringify(newDataSheet), function (err) {if (err) throw err;console.log('Updated!');});
+        console.log("New User Created");
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+function readUserDataSheet(username){
+    var fs = require('fs');
+    try {
+        console.log("readinguserdata:");
+        
+        console.log(JSON.parse(fs.readFileSync('UserData/'+username+'.txt', 'utf8')));
+        return JSON.parse(fs.readFileSync('UserData/'+username+'.txt', 'utf8'));
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
